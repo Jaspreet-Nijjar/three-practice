0e896f0dbf568a73aa700a3b9f035636b1d30e09
Jaspreet <jaspreetkn92@gmail.com>
2023-06-20 17:19:45 +0100

    added lights

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 import * as THREE from 'three';
-
+//Creating a scene
 const scene = new THREE.Scene();
 
+//Creating a sphere
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({ color: '#00ff83' });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+//Lights
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
