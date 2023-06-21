92ba43217e9fb32878a3d81df70837b35932830e
Jaspreet <jaspreetkn92@gmail.com>
2023-06-21 18:06:56 +0100

    added timeline to  the navbar

--- main.js
+++ main.js
@@ -58,3 +58,4 @@ animation();
 //Adding timeline
 const tl = gsap.timeline({ defaults: { duration: 1 } });
 tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+tl.fromTo('nav', { y: '-100%' }, { y: '0%' });
